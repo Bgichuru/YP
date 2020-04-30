ec6340a94cbe9469a1f3fffb96df80acc760ded5
Bgichuru <chege.gichuru@gmail.com>
2020-04-30 15:37:16 +0300

    Add files via upload

---
+++ shopping-cart/shopping-cart-master/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
+import cartReducer from './components/reducers/cartReducer';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import thunk from "redux-thunk";
+import {applyMiddleware, compose} from "redux";
+
+const initialState = {
+    items: [],
+    addedItems:[],
+    total: 0
+};
+
+const middleware = [thunk];
+
+const store = createStore(
+    cartReducer,
+    initialState,
+    compose(
+        applyMiddleware(...middleware)
+    )
+);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
